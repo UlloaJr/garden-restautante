aebb8ff485010bf5df2a21f3bdd546d6c68c2dbe
Jorge Ulloa <108245001+UlloaJr@users.noreply.github.com>
2024-01-08 17:18:30 -0300

    Add files via upload

--- Grilli/assets/js/script.js
+++ Grilli/assets/js/script.js
@@ -46,10 +46,11 @@ addEventListener(navTogglers, "click", toggleNavbar);
 
 
 /**
- * HEADER
+ * HEADER & BACK TOP BTN
  */
 
 const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
 
 let lastScrollPos = 0;
 
@@ -57,8 +58,10 @@ const hideHeader = function () {
     const isScrollBottom = lastScrollPos < window.scrollY;
     if (isScrollBottom) {
         header.classList.add("hide");
+        backTopBtn.classList.add("hide");
     } else {
         header.classList.remove("hide");
+        backTopBtn.classList.remove("hide");
     }
 
     lastScrollPos = window.scrollY;
@@ -67,9 +70,11 @@ const hideHeader = function () {
 window.addEventListener("scroll", function () {
     if (window.scrollY > 50) {
         header.classList.add("active");
+        backTopBtn.classList.add("active");
         hideHeader();
     } else {
         header.classList.remove("active");
+        backTopBtn.classList.remove("active");
     }
 });
 
